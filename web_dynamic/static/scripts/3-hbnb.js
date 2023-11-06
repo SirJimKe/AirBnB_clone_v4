7ecef35133ca24ef76dcbd0f17e46e0a08da4b39
valariembithe <valmbithe691@gmail.com>
2023-11-06 15:13:10 +0300

    Uses POST method to update article on places

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,74 +1,65 @@
 $(document).ready(function () {
     const selectedAmenities = {};
     fetchPlaces();
-    
     $('.amenity-checkbox').change(function () {
-      const amenityId = $(this).closest('li').data('id');
-      const amenityName = $(this).closet('li').data('name');
+      const id = $(this).data('id');
+      const name = $(this).data('name');
   
-      if ($(this).prop('checked')) {
-        selectedAmenities[amenityId] = amenityName;
+      if ($(this).is(':checked')) {
+        selectedAmenities[id] = name;
       } else {
-        delete selectedAmenities[amenityId];
+        delete selectedAmenities[id];
       }
+      $('.amenities h4').text(Object.values(selectedAmenities).join(', '));
   
-      const amenitiesList = Object.values(selectedAmenities).join(', ');
-      $('.amenities h4').text('Amenities: ' + amenitiesList);
     });
-
+  
+ 
     $.ajax({
         url: "http://0.0.0.0:5001/api/v1/status/",
         type: "GET",
     }).done (function (response) {
         if (response.status === 'OK') {
-            $("div#api_status").addClass('available');
-            console.log(response.status);
+            $('#api_status').addClass('available');
+            console.log(response);
         } else {
-            $("div#api_status").removeClass('available');
+            $('#api_status').removeClass('available');
         }
     });
 });
-
-const shortenString = (string, length = 30) => {
-    if (string.length <= length) return string;
-  
-    return string.substring(0, length - 3) + "...";
-  };
-
-  function fetchPlaces(filter = {}) {
+function fetchPlaces(filter={}) {
     const options = {
-        url:  "http://0.0.0.0:5001/api/v1/places_search/",
-        contentType: "application/json",
-        data: JSON.stringify(filter),
-        type: POST,
+        type: 'POST',
+        url: 'http://0.0.0.0:5001/api/v1/places_search/',
+        contentType: application/json,
+        data: JSON.stringify({filter}),
     };
-    $.ajax(options).done (function (response) {
+    $.ajax(options).done(function (response) {
         $("section.places").empty();
         response.forEach((place) => {
             const placeMarkup = $(`
             <article>
-            <div class="title_box">
-              <h2>${place.name}</h2>
-              <div class="price_by_night">$${place.price_by_night}</div>
-            </div>
-            <div class="information">
-              <div class="max_guest">
-                ${place.max_guest} ${place.max_guest > 1 ? "Guests" : "Guest"}
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">$${place.price_by_night}</div>
               </div>
-              <div class="number_rooms">
-                ${place.number_rooms} ${place.max_guest > 1 ? "Bedrooms" : "Bedroom"}
+              <div class="information">
+                <div class="max_guest">
+                  ${place.max_guest} ${place.max_guest > 1 ? "Guests" : "Guest"}
+                </div>
+                <div class="number_rooms">
+                  ${place.number_rooms} ${place.max_guest > 1 ? "Bedrooms" : "Bedroom"}
+                </div>
+                <div class="number_bathrooms">
+                ${place.number_bathrooms} ${
+                  place.number_bathrooms > 1 ? "Bathrooms" : "Bathroom"
+                }
+                </div>
               </div>
-              <div class="number_bathrooms">
-              ${place.number_bathrooms} ${
-                place.number_bathrooms > 1 ? "Bathrooms" : "Bathroom"
-              }
-              </div>
-            </div>
-            <div class="description">${place.description}</div>
-          </article>
-            `);
-
-            $("section.places").append("placeMarkup");
+              <div class="description">${place.description}</div>
+            </article>
+              `);          
+            $("section.places").append(placeMarkup);
         });
     });
 }

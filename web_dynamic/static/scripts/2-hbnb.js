a8035982a25a28d00619f333d90e6b1eae29489e
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-11-07 04:55:40 +0300

    available status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,28 +1,25 @@
 $(document).ready(function () {
-    const selectedAmenities = {};
-    $('.amenity-checkbox').change(function () {
-      const id = $(this).data('id');
-      const name = $(this).data('name');
-  
-      if ($(this).is(':checked')) {
-        selectedAmenities[id] = name;
-      } else {
-        delete selectedAmenities[id];
-      }
-      $('.amenities h4').text(Object.values(selectedAmenities).join(', '));
-  
-    });
-  
- 
-    $.ajax({
-        url: "http://0.0.0.0:5001/api/v1/status/",
-        type: "GET",
-    }).done (function (response) {
-        if (response.status === 'OK') {
+    function updateAmenitiesList() {
+        const selectedAmenities = {};
+        $('.amenity-checkbox').each(function () {
+            const id = $(this).data('id');
+            const name = $(this).data('name');
+            if ($(this).is(':checked')) {
+                selectedAmenities[id] = name;
+            }
+        });
+        $('.amenities h4').text(Object.values(selectedAmenities).join(', '));
+    }
+
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+        if (data.status === 'OK') {
             $('#api_status').addClass('available');
-            console.log(response);
         } else {
             $('#api_status').removeClass('available');
         }
+    });
+
+    $('.amenity-checkbox').change(function () {
+        updateAmenitiesList();
     });
 });
